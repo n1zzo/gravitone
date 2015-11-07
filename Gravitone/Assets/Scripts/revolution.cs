88095da199fce19897ddb082b9cf69fe2f4804a9
Niccolò Izzo <izzo.niccolo@gmail.com>
2015-11-07 12:04:07 +0100

    Refactored the code, now it is no more based on angles. Partially added support for use input.

--- Gravitone/Assets/Scripts/revolution.cs
+++ Gravitone/Assets/Scripts/revolution.cs
@@ -10,42 +10,68 @@ public class revolution : MonoBehaviour {
 	public int beatsPerBar = 4;
 	public float radius = 1f;
 	const float TWO_PI = 2*Mathf.PI;
-	float currentAngle = 0;
-	float angularSpeed = 0;
-	float error = 0.1f;
-	AudioSource kick;
+  const int TOT_SLOTS = 32;
+  // While progress goes from 0 to 1 we complete one bar
+  float progress = 0f;
+  float timeSpeed = 0f;
+	float currentAngle = 0f;
+	float error = 0.02f;
+  bool[] slots = new bool[TOT_SLOTS];
+	AudioSource sound;
 
   // Use this for initialization
 	void Start () {
+
+		slots[16] = true;
+
 		// Gets the x and y coordinates and bpm from the reference star
 		starX = star.GetComponent<star>().x;
 		starY = star.GetComponent<star>().y;
 		bpm = star.GetComponent<star>().bpm;
 
-		// Loads the kick clip
-		kick = GetComponent<AudioSource>();
+		// Loads the drum clip
+		sound = GetComponent<AudioSource>();
 
 		// Sets the initial position
 		transform.position = new Vector3(0, radius, 0);
 
-		// Angular speed is derived from BPMs
-		angularSpeed = bpm * TWO_PI	/ (60 * beatsPerBar);
-		
+    // Time speed is derived from BPMs
+    timeSpeed = (float) bpm / (60 * (float) beatsPerBar);
+
 		// The first beat is always missing
 		// We trigger it manually at initialization
-		kick.Play();
+  	sound.Play();
 	}
 
   // Update is called once per frame
   void Update () {
-		// At every frame the planet's position is updated
-		currentAngle += angularSpeed * Time.deltaTime;
+    // Update the bar's progress
+    progress += timeSpeed * Time.deltaTime;
+
+    // Avoid progress overflow
+    if (progress >= 1)
+      progress -= 1;
+
+    // Calculate the planet's position
+    currentAngle = progress * TWO_PI;
 		transform.position = getPosition(currentAngle);
 
-		// Plays the kick only if it's not yet playing
-		if (!kick.isPlaying && checkBeat(currentAngle)) {
-			kick.Play();
+		if (Input.GetKeyDown("space"))
+      // Records in the array that you pressed a button
+      print("space key was pressed");
+      slots[(int) (progress / TOT_SLOTS)] = true;
+
+    /*
+		// Plays the sound at every beat (only if it's not playing)
+		if (!sound.isPlaying && checkBeat(progress) < error) {
+			sound.Play();
 		}
+    */
+
+    // Plays the sound if the current slot is full
+    if (!sound.isPlaying && checkSlot(progress)) {
+      sound.Play();
+    }
 	}
 
 	// Obtains the planet position from the planet's angle
@@ -54,8 +80,12 @@ public class revolution : MonoBehaviour {
 	}
 
 	// Checks if the current angle corresponds to a beat within a given error range
-	bool checkBeat (float angle) {
-		return (angle % (TWO_PI / beatsPerBar) < error);
+ 	float checkBeat (float progress) {
+		return progress % (1 / (float) beatsPerBar);
 	}
+
+  bool checkSlot (float progress) {
+  	return slots[(int) (progress * (float) TOT_SLOTS)];
+  }
 
 }
