87bb8b73e8d7a23c81dae06d075ef37369680a5f
Giuseppe Di Francesco <giusep502@gmail.com>
2015-11-12 22:09:40 +0100

    Added LIBPD load patch and loaded metronome
    Also the Patch metronome has been modified to make it function with the
    loader.
    PROBLEM: If libpd is on, other sounds won't be played... so we must find
    a way to play them inside libpd, and i readed that it won't function
    well on Android..

---
+++ Gravitone/Assets/Scripts/LoadMetronome.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using LibPDBinding;
+
+public class LoadMetronome : MonoBehaviour {
+
+	bool start=false;
+
+	// Use this for initialization
+	void Start () {
+
+			LibPD.SendFloat("metroSound", 80f);
+			LibPD.SendBang("metroControl");
+			
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
