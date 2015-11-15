680e301f0dbba7c3660f135e40515037b60ac156
Giuseppe Di Francesco <giusep502@gmail.com>
2015-11-15 19:12:05 +0100

    Fixed Buttons
    :dancer: :D :dancers:

---
+++ Gravitone/Assets/Scripts/ChangeButtonShape.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeButtonShape : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void hide(){
+		this.gameObject.SetActive(false);
+	}
+
+	public void show(){
+		this.gameObject.SetActive(false);
+	}
+}
