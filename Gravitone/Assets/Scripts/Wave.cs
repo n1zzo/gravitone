286da79448259ac3a0738be352902065293caec5
Niccolò Izzo <izzo.niccolo@gmail.com>
2015-11-21 21:25:44 +0100

    Edited Wave.cs class.

--- Gravitone/Assets/Scripts/Wave.cs
+++ Gravitone/Assets/Scripts/Wave.cs
@@ -1,18 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Wave : MonoBehaviour {
+public class Wave : Subscriber {
 
 	float radius = 0f;
-	float scaleSpeed = .005f;
+	float scaleSpeed = 1f;
+	public GameObject star;
+	int subBeatsPerBeat = 4;
 
 	// Use this for initialization
 	void Start () {
-
+		subBeatsPerBeat = star.GetComponent<BeatGen>().subBeatsPerBeat;
+		star.GetComponent<BeatGen>().Subscribe(this);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		transform.localScale += new Vector3(scaleSpeed, scaleSpeed, 0);
+		//transform.localScale += new Vector3(scaleSpeed, scaleSpeed, 0);
+	}
+
+	// This method is called for each beat
+	public virtual void Beat(int currentSlot) {
+		// check every bar if the array is correct
+		if(currentSlot%subBeatsPerBeat == 0)
+			Debug.Log("LOL");
+			transform.localScale += new Vector3(scaleSpeed, scaleSpeed, 0);
 	}
 }
