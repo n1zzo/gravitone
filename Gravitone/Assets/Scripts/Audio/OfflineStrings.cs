00f43978657182ea02f3d42531364c01e8ea47a7
Niccolò Izzo <izzo.niccolo@gmail.com>
2015-12-11 17:34:47 +0100

    Added semitone to pitch conversion algorithm.

--- Gravitone/Assets/Scripts/Audio/OfflineStrings.cs
+++ Gravitone/Assets/Scripts/Audio/OfflineStrings.cs
@@ -3,7 +3,15 @@ using System.Collections;
 
 public class OfflineStrings : MonoBehaviour, Strings {
 
+	private int sampleNote = 60;
+	private float mFactor = 1.0594631f;
+	public AudioSource sample;
+
 	void Strings.Play(int note) {
-		//[TODO] Implement offline version of Strings
+		// This changes the audiosource shift to match the target note
+		int shift = note - sampleNote;
+		float newpitch = Mathf.Pow(mFactor, (float)shift);
+		sample.pitch = newpitch;
+		sample.Play();
 	}
 }
