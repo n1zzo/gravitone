200695b05a68d68786c391b809f6e8d440176c26
Niccolò Izzo <izzo.niccolo@gmail.com>
2015-12-13 15:53:37 +0100

    Added melody handling code.

---
+++ Gravitone/Assets/Scripts/Objects/Melodies.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Melodies : Subscriber {
+
+	public GameObject star;
+	private int[] melodyNotes = new int[64];
+	private int[] playerNotes = new int[64];
+	private int currentSlot;
+	public GameObject audioManager;
+
+	// Use this for initialization
+	void Start () {
+		// Subscribe to the star
+		star.GetComponent<BeatGen>().Subscribe(this);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// currentSlot ranges from 0 to 15
+	public override void Beat(int currentSlot){
+		this.currentSlot = currentSlot;
+		// Play the user's saved note
+		int noteToPlay = playerNotes[currentSlot];
+		if(noteToPlay != -1)
+			audioManager.GetComponent<AudioManager>().PlayStrings(noteToPlay);
+	}
+
+	public void RecordNote(int note) {
+		playerNotes[currentSlot] = note;
+		Verify();
+	}
+
+	private void Verify() {
+
+	}
+
+}
