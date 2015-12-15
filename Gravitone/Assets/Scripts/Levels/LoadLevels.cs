d0eaa0bf54945d27984b7bc2ad96b398439af045
Giuseppe Di Francesco <giusep502@gmail.com>
2015-12-15 11:58:22 +0100

    Added Temporary menu

---
+++ Gravitone/Assets/Scripts/Levels/LoadLevels.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevels : MonoBehaviour {
+
+	public void NextLevelButton(string levelName)
+     {
+         Application.LoadLevel(levelName);
+     }
+		 
+}
