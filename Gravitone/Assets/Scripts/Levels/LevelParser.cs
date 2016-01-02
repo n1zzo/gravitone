000144931ac499b5da6d568c4a40e38e449f8c82
Stocarson <izzo.niccolo@gmail.com>
2016-01-02 18:09:00 +0100

    Added JSON level loading.

---
+++ Gravitone/Assets/Scripts/Levels/LevelParser.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class LevelParser : MonoBehaviour {
+
+	public int number = 1;
+	public GameObject planetKick;
+	public GameObject planetSnare;
+	public GameObject planetHat;
+	private string toParse;
+
+	// Use this for initialization
+	void Start () {
+		 LoadLevel();
+	}
+
+	void LoadLevel() {
+		TextAsset currentLevel = Resources.Load("Levels/Level"+number) as TextAsset;
+		toParse = currentLevel.text;
+		var N = JSON.Parse(toParse);
+		//Debug.Log(N);
+		for (int i = 0; i < 64; i++) {
+			Debug.Log(N["data"]["kickArray"][i]);
+			if (N["data"]["kickArray"][i].AsInt == 0)
+				planetKick.GetComponent<Drum>().targetDrumArray[i] = false;
+			else
+				planetKick.GetComponent<Drum>().targetDrumArray[i] = true;
+		}
+		for (int i = 0; i < 64; i++) {
+			if (N["data"]["snareArray"][i].AsInt == 0)
+				planetSnare.GetComponent<Drum>().targetDrumArray[i] = false;
+			else
+				planetSnare.GetComponent<Drum>().targetDrumArray[i] = true;
+		}
+		for (int i = 0; i < 64; i++) {
+			if (N["data"]["hatArray"][i].AsInt == 0)
+				planetHat.GetComponent<Drum>().targetDrumArray[i] = false;
+			else
+				planetHat.GetComponent<Drum>().targetDrumArray[i] = true;
+		}
+	}
+
+}
