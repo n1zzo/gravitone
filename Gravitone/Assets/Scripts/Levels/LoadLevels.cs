dd79a7b0c321f768b442a5a9e60d32b0ab2ae91f
Niccolò Izzo <izzo.niccolo@gmail.com>
2016-01-16 17:32:11 +0100

    Added menu.

--- Gravitone/Assets/Scripts/Levels/LoadLevels.cs
+++ Gravitone/Assets/Scripts/Levels/LoadLevels.cs
@@ -4,9 +4,21 @@ using System.Collections;
 
 public class LoadLevels : MonoBehaviour {
 
+    public GameObject title;
+    public GameObject playButton;
+    public GameObject[] levelButtons;
     public GameObject globals;
 
-	public void NextLevelButton(int level) {
+    public void ShowLevels() {
+        title.SetActive(false);
+        playButton.SetActive(false);
+        Debug.Log("LOL");
+        foreach(GameObject button in levelButtons) {
+                button.SetActive(true);
+        }
+    }
+
+	public void StartLevel(int level) {
          globals.GetComponent<Globals>().SetLevelNumber(level);
          Application.LoadLevel("LevelTemplate");
     }
