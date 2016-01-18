d0d25ac5d7f6c25a86d4af66086d138ea64716b6
Giuseppe Di Francesco <giusep502@gmail.com>
2016-01-19 00:15:05 +0100

    Created Metronome Prefab

---
+++ Gravitone/Assets/Scripts/Various/MenuMetronome.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using LibPDBinding;
+using System.Collections;
+
+public class MenuMetronome : Subscriber {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<BeatGen>().Subscribe(this);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public override void Beat(int cs) {
+		if(cs==0)
+			LibPD.SendBang("highBeat");
+	}
+
+}
