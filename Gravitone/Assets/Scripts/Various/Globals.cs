83607e6ff72fd89d0026fddea1a79a3d4b2e964a
Giuseppe Di Francesco <giusep502@gmail.com>
2016-01-18 23:49:51 +0100

    Return to Main Menu button done!

--- Gravitone/Assets/Scripts/Various/Globals.cs
+++ Gravitone/Assets/Scripts/Various/Globals.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class Globals : MonoBehaviour {
 
     public int number = 0;
-    public int bpm;
-    public int beatsPerBar;
-    public int subBeatsPerBeat;
+    public int bpm=128;
+    public int beatsPerBar=4;
+    public int subBeatsPerBeat=4;
 
 	// Use this for initialization
 	void Start () {
