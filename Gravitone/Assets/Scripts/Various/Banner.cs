6ed292557a9f989d0165ec127c8ab45bd3d0b9c0
Niccolò Izzo <izzo.niccolo@gmail.com>
2016-01-29 14:49:34 +0100

    Added game logo.

--- Gravitone/Assets/Scripts/Various/Banner.cs
+++ Gravitone/Assets/Scripts/Various/Banner.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class Banner : MonoBehaviour {
 
+    public float time = 3.0f;
+
 	// Use this for initialization
 	void Start () {
 		StartCoroutine(HideBanner());
+        StartCoroutine(LoadMenu());
 	}
 
 	IEnumerator HideBanner() {
-			yield return new WaitForSeconds(3.0f);
+			yield return new WaitForSeconds(time);
+            this.GetComponent<SpriteRenderer>().enabled = false;
+	}
+
+    IEnumerator LoadMenu() {
+			yield return new WaitForSeconds(time*2);
 			Application.LoadLevel("menu");
 	}
 }
